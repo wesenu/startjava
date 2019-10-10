9d2aa1a6b2ec5c1e094aceb00a919029aa5dea6f
Igor Kopyshev <igor.k.engi@gmail.com>
2019-10-10 14:17:10 +0300

    HW_1: ConditionalStatement - Fixed variables.

--- lesson_1/ConditionalStatement.java
+++ lesson_1/ConditionalStatement.java
@@ -1,14 +1,11 @@
 class ConditionalStatement {
 	public static void main(String[] args) {
-		int age = 24;
-		boolean isMale = true;
-		double height = 1.83;
-		char firstLetterOfYourName = 'I';
-
+		int age = 24;		
 		if (age > 20) {
 			System.out.println("You're over 20 years old!");
 		}
 
+		boolean isMale = true;
 		if (isMale) {
 			System.out.println("You're a male. Congratulations.");
 		}
@@ -17,13 +14,15 @@ class ConditionalStatement {
 			System.out.println("Unfortunately you're not a male");
 		}
 
+		double height = 1.83;
 		if (height > 1.80) {
 			System.out.println("You're taller than me. Maybe.");
 		}
 
-		if (firstLetterOfYourName == 'M') {
+		char firstLetterOfName = 'I';
+		if (firstLetterOfName == 'M') {
 			System.out.println("For sure! I don't know what can I say here. You're exactly not me XD");
-		} else if (firstLetterOfYourName == 'I') {
+		} else if (firstLetterOfName == 'I') {
 			System.out.println("Oh, your name may be the same as mine. Not sure about that.");
 		} else {
 			System.out.println("Like that?! Who are you, buddy?");
