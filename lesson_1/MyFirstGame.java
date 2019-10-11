2cf603536c21aeb86efafc072fd91d949920f84d
Igor Kopyshev <igor.k.engi@gmail.com>
2019-10-11 15:50:46 +0300

    HW_1: MyFirstGame has been simplified and fixed

--- lesson_1/MyFirstGame.java
+++ lesson_1/MyFirstGame.java
@@ -1,26 +1,18 @@
-import java.io.*;
 public class MyFirstGame {
 	public static void main(String[] args) {
-		BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 		int guessedNumber = 85; 
-		boolean exit = false;
-		int typedNumber = 0;
-
-		System.our.println("Type any number")
-		while (!exit) {
-			try {
-				typedNumber = Integer.parseInt(consoleReader.readLine());
-				if (typedNumber > guessedNumber) {
-					System.out.println("Your number is greater than what the computer selected.");
-				} else if (typedNumber < guessedNumber) {
-					System.out.println("Your number is less than what the computer made.");
-				} else {
-					System.out.println("You guessed!");
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (NumberFormatException e) {
-				System.out.println("That's not a number. Try again please.");
+		int playerNumber = 91;
+		boolean isExit = false;
+		while (!isExit) {
+			if (playerNumber > guessedNumber) {
+				System.out.println("Your number is greater than what the computer selected.");
+				playerNumber -= 15;
+			} else if (playerNumber < guessedNumber) {
+				System.out.println("Your number is less than what the computer made.");
+				playerNumber += 31;
+			} else {
+				System.out.println("You guessed!");
+				isExit = true;
 			}
 		}
 	}
