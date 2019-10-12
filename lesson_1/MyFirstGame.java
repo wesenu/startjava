cef23b475345ae4d1e533e6871d7c2341507758a
Igor Kopyshev <igor.k.engi@gmail.com>
2019-10-12 18:01:05 +0300

    HW_1: MyFirstGame - Win condition has been fixed.

--- lesson_1/MyFirstGame.java
+++ lesson_1/MyFirstGame.java
@@ -2,18 +2,16 @@ public class MyFirstGame {
 	public static void main(String[] args) {
 		int guessedNumber = 85; 
 		int playerNumber = 91;
-		boolean isExit = false;
-		while (!isExit) {
+		
+		while (guessedNumber != playerNumber) {
 			if (playerNumber > guessedNumber) {
 				System.out.println("Your number is greater than what the computer selected.");
 				playerNumber -= 15;
 			} else if (playerNumber < guessedNumber) {
 				System.out.println("Your number is less than what the computer made.");
 				playerNumber += 31;
-			} else {
-				System.out.println("You guessed!");
-				isExit = true;
-			}
+			}		
 		}
+		System.out.println("You guessed!");	
 	}
 }
