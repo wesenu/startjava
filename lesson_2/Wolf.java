6c7c61d1fd2136e7a47123f25a8011b3382c0bcb
Igor Kopyshev <igor.k.engi@gmail.com>
2019-10-20 16:56:18 +0300

    HW_2: Refactoring in Wolf.java

--- lesson_2/Wolf.java
+++ lesson_2/Wolf.java
@@ -5,6 +5,50 @@ class Wolf {
 	private String color;
 	private String sex;
 
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (age > 8) {
+			System.out.println("Incorrect age");
+		} else {
+			this.age = age;
+		}
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeigth(float weight) {
+		this.weight = weight;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
 	public void go() {
 		System.out.println(name + " is going somewhere...");
 	}
@@ -23,49 +67,5 @@ class Wolf {
 
 	public void hunt() {
 		System.out.println("*" + name  + " is hunting... Looks scary, btw.*");
-	}
-
-	public void setAge(int age) {
-		if (age > 8) {
-			System.out.println("Incorrect age");
-		} else {
-			this.age = age;
-		}
-	}
-
-	public void setWeigth(float weight) {
-		this.weight = weight;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setColor(String color) {
-		this.color = color;
-	}
-
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-
-	public int getAge() {
-		return this.age;
-	}
-
-	public float getWeight() {
-		return this.weight;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public String getColor() {
-		return this.color;
-	}
-
-	public String getSex() {
-		return this.sex;
 	}
 }
