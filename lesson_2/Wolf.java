f12dc638fb14eb76d5f8fa4b449f52a1fc1015da
Igor Kopyshev <igor.k.engi@gmail.com>
2019-10-20 16:31:31 +0300

    HW_2: Wolf.java and WolfTest.java has been modified

--- lesson_2/Wolf.java
+++ lesson_2/Wolf.java
@@ -1,9 +1,9 @@
 class Wolf {
-	int age;
-	float weight;
-	String name;
-	String color;
-	String sex;
+	private int age;
+	private float weight;
+	private String name;
+	private String color;
+	private String sex;
 
 	public void go() {
 		System.out.println(name + " is going somewhere...");
@@ -23,5 +23,49 @@ class Wolf {
 
 	public void hunt() {
 		System.out.println("*" + name  + " is hunting... Looks scary, btw.*");
+	}
+
+	public void setAge(int age) {
+		if (age > 8) {
+			System.out.println("Incorrect age");
+		} else {
+			this.age = age;
+		}
+	}
+
+	public void setWeigth(float weight) {
+		this.weight = weight;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public float getWeight() {
+		return this.weight;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getColor() {
+		return this.color;
+	}
+
+	public String getSex() {
+		return this.sex;
 	}
 }
--- lesson_2/WolfTest.java
+++ lesson_2/WolfTest.java
@@ -1,16 +1,16 @@
 class WolfTest {
 	public static void main(String[] args) {
 		Wolf myWolf = new Wolf();
-		myWolf.name = "Sharik";
-		myWolf.sex = "male";
-		myWolf.color = "yellow";
-		myWolf.age = 10;
-		myWolf.weight = 2.50f;
+		myWolf.setName("Sharik");
+		myWolf.setSex("male");
+		myWolf.setColor("yellow");
+		myWolf.setAge(7);
+		myWolf.setWeigth(2.50f);
 		
-		System.out.println("Wolf's name is " + myWolf.name + "\n"
-			+ "He is " + myWolf.color +  " " + myWolf.sex + "\n"
-			+ "He is " + myWolf.age + " years old" + "\n"
-			+ "And he weights " + myWolf.weight + " tons." + "\n");
+		System.out.println("Wolf's name is " + myWolf.getName() + "\n"
+			+ "He is " + myWolf.getColor() +  " " + myWolf.getSex() + "\n"
+			+ "He is " + myWolf.getAge() + " years old" + "\n"
+			+ "And he weights " + myWolf.getWeight() + " tons." + "\n");
 
 		myWolf.go();
 		myWolf.sit();
