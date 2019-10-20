5f9b714bd3b5e923c99664e50118f88d9f6377f9
Igor Kopyshev <igor.k.engi@gmail.com>
2019-10-20 12:23:40 +0300

    HW_2: Small refactoring in Calculator.java

--- lesson_2/Calculator.java
+++ lesson_2/Calculator.java
@@ -1,45 +1,42 @@
-class Calculator {
-	private int result = 0;
-		
+class Calculator {	
 	public int calculate(int firstNumber, int secondNumber, char mathOperation) {
 		switch(mathOperation) {
-			case '+': result = addition(firstNumber, secondNumber); break; 
-			case '-': result = substraction(firstNumber, secondNumber); break;
-			case '*': result = multiplication(firstNumber, secondNumber); break;
-			case '/': result = division(firstNumber, secondNumber); break;
-			case '^': result = exponentiation(firstNumber, secondNumber); break; 
-			case '%': result = divisionByReminder(firstNumber, secondNumber); break;
+			case '+': return add(firstNumber, secondNumber); 
+			case '-': return substract(firstNumber, secondNumber); 
+			case '*': return multiply(firstNumber, secondNumber); 
+			case '/': return divide(firstNumber, secondNumber); 
+			case '^': return exponentiate(firstNumber, secondNumber); 
+			case '%': return divideByReminder(firstNumber, secondNumber); 
 			default: System.out.println("Oops! Invalid math operation..." + "\n");
 				return -1;
 		}
-		return result;
 	}
 
-	private int addition(int firstNumber, int secondNumber) {
+	private int add(int firstNumber, int secondNumber) {
 		return firstNumber + secondNumber;
 	}
 
-	private int substraction(int firstNumber, int secondNumber) {
+	private int substract(int firstNumber, int secondNumber) {
 		return firstNumber - secondNumber;
 	}
 
-	private int multiplication(int firstNumber, int secondNumber) {
+	private int multiply(int firstNumber, int secondNumber) {
 		return firstNumber * secondNumber;
 	}
 
-	private int division(int firstNumber, int secondNumber) {
+	private int divide(int firstNumber, int secondNumber) {
 		return firstNumber / secondNumber;
 	}
 
-	private int exponentiation(int firstNumber, int secondNumber) {
-		result = 1;
+	private int exponentiate(int firstNumber, int secondNumber) {
+		int result = 1;
 		for (int i = 0; i < secondNumber; i++) {
 			result *= firstNumber;
 		}
 		return result;
 	}
 
-	private int divisionByReminder(int firstNumber, int secondNumber) {
+	private int divideByReminder(int firstNumber, int secondNumber) {
 		return firstNumber % secondNumber;
 	}
 }
